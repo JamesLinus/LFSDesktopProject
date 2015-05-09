c11fdc9921f019fde2ce23aaa66679d646e26c2d
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-05-09 19:36:38 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@47 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

--- LFSDesktop/LFSDesktop/src/graphics.cpp
+++ LFSDesktop/LFSDesktop/src/graphics.cpp
@@ -23,37 +23,27 @@ void drawImage(char *type,char *label,int x,int y,bool mounted)
 	ENTRY	*retentry=NULL;
 	char	*tstr;
 
+	if(strcasecmp(type,"sata")==0)
+		{
+			testentry.key=(char*)"sata";
+			testentry.data=NULL;
+			retentry=hsearch(testentry,ENTER);
+			if(retentry->data==NULL)
+				{
+					retentry->data=malloc(sizeof(diskIconStruct));
+					tstr=pathToIcon("harddisk");
+					makeImage((char*)tstr,(char*)"sata",SATA,(diskIconStruct*)(retentry->data));
+					free(tstr);
+				}
+		}
+
 	if(mounted==true)
 		{
 			if(strcasecmp(type,"sata")==0)
 				{
-					//ENTRY sataitem={"sata",NULL};
-					//ENTRY *retentry=NULL;
-					testentry.key=(char*)"sata";
-					testentry.data=NULL;
-					retentry=hsearch(testentry,ENTER);
-					if(retentry->data==NULL)
-						{
-						printf("XXXXXXXX\n");
-							retentry->data=malloc(sizeof(diskIconStruct));
-							tstr=pathToIcon("harddisk");
-							makeImage((char*)tstr,(char*)"sata",SATA,(diskIconStruct*)(retentry->data));
-							free(tstr);
-						}
-					//retentry=hsearch(testentry,FIND);
-					XSetClipMask(display,gc,diskIconsPixmapMask[SATA][0]);
-					//XSetClipMask(display,gc,((diskIconStruct*)retentry->data)->mask);
+					XSetClipMask(display,gc,((diskIconStruct*)retentry->data)->mask);
 					XSetClipOrigin(display,gc,x,y);
-					//XCopyArea(display,diskIconsPixmap[SATA][0],drawOnThis,gc,0,0,iconSize,iconSize,x,y);
-				//	printf("+++%i+++\n",((diskIconStruct*)retentry->data)->pixmap);
-				//	printf(">>%s<<\n",((diskIconStruct*)retentry->data)->name);
-				//long unsigned int *xx=(long unsigned int*)(((diskIconStruct*)retentry->data)->pixmap);
-					//Pixmap pm=((diskIconStruct*)retentry->data)->pixmap;
-					//printf(">>%i<<\n",pm);
-					//pm=diskIconsPixmap[SATA][0];
-					//printf("--%i--\n",pm);
-					
-	XCopyArea(display,((diskIconStruct*)retentry->data)->pixmap,drawOnThis,gc,0,0,iconSize,iconSize,x,y);
+					XCopyArea(display,((diskIconStruct*)retentry->data)->pixmap,drawOnThis,gc,0,0,iconSize,iconSize,x,y);
 				}
 			if(strcasecmp(type,"usb")==0)
 				{
