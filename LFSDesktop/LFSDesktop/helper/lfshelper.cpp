dfebbfe32ae13c2a5ef8342470e6b96851534a73
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-05-12 10:59:29 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@57 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

---
+++ LFSDesktop/LFSDesktop/helper/lfshelper.cpp
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello World!\n");
+	printf(">>%s<,\n",HELPERDIR);
+	return 0;
+}
--- LFSDesktop/LFSDesktop/src/main.cpp
+++ LFSDesktop/LFSDesktop/src/main.cpp
@@ -111,7 +111,8 @@ int main(int argc,char **argv)
 	Time			time=0;
 	bool			firstClick=false;
 	bool			foundIcon=false;
-
+printf(">>%s<<\n",HELPERDIR);
+printf(">>%s<<\n",HELPERAPP);
 	asprintf(&path,"%s/.config/LFS/pidfile",getenv("HOME"));
 	fw=fopen(path,"r");
 	if(fw!=NULL)
