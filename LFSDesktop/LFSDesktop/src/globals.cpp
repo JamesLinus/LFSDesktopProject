7139c27506defcecbf8a9b8bbfed719999ee5587
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-05-27 13:17:44 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@80 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

---
+++ LFSDesktop/LFSDesktop/src/globals.cpp
@@ -0,0 +1,52 @@
+/******************************************************
+*
+*     ©keithhedger Fri 22 May 14:47:56 BST 2015
+*     kdhedger68713@gmail.com
+*
+*     globals.cpp
+* 
+******************************************************/
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "globals.h"
+
+int			errLine;
+const char	*errFile;
+const char	*errFunc;
+
+//save/load file
+//global file data
+char		*fileDiskLabel=NULL;
+char		*fileDiskMime=NULL;
+char		*fileDiskPath=NULL;
+char		*fileDiskUUID=NULL;
+char		*fileDiskType=NULL;
+int			fileDiskXPos=-1;
+int			fileDiskYPos=-1;
+
+int			foundDiskNumber=-1;
+bool		isDisk=false;
+
+int			xCnt;
+int			yCnt;
+
+args		globalFileData[]=
+{
+	{"label",TYPESTRING,&fileDiskLabel},
+	{"mime",TYPESTRING,&fileDiskMime},
+	{"path",TYPESTRING,&fileDiskPath},
+	{"uuid",TYPESTRING,&fileDiskUUID},
+	{"type",TYPESTRING,&fileDiskType},
+	{"xpos",TYPEINT,&fileDiskXPos},
+	{"ypos",TYPEINT,&fileDiskYPos},
+
+	{NULL,0,NULL}
+};
+
+//file icon data
+fileInfo	*disksDataPtr;
+int			desktopFilesCnt=0;
+int			desktopFilesCntMax=0;
---
+++ LFSDesktop/LFSDesktop/src/globals.h
@@ -0,0 +1,65 @@
+/******************************************************
+*
+*     ©keithhedger Fri 22 May 14:47:56 BST 2015
+*     kdhedger68713@gmail.com
+*
+*     globals.h
+* 
+******************************************************/
+
+#ifndef _GLOBALS_
+#define _GLOBALS_
+
+#define DEBUGSTR(x) errLine=__LINE__,errFile=__FILE__,errFunc=__func__;printString((char*)x)
+#define DEBUGVAL(x) errLine=__LINE__,errFile=__FILE__,errFunc=__func__;printInt((int)x)
+#define debugstr(x) DEBUGSTR(x)
+
+enum {TYPEINT=1,TYPESTRING,TYPEBOOL};
+enum {CACHEFOLDER,DISKFOLDER};
+
+struct args
+{
+	const char*	name;
+	int			type;
+	void*		data;
+};
+
+struct fileInfo
+{
+	char		*label;
+	char		*mime;
+	char		*path;
+	char		*icon;
+	char		*uuid;
+	char		*type;
+	int			x;
+	int			y;
+};
+
+extern int			errLine;
+extern const char	*errFile;
+extern const char	*errFunc;
+
+//file icon data
+extern fileInfo	*disksDataPtr;
+extern int		desktopFilesCnt;
+extern int		desktopFilesCntMax;
+
+//save/load file
+extern args		globalFileData[];
+//global file data
+extern char		*fileDiskLabel;
+extern char		*fileDiskMime;
+extern char		*fileDiskPath;
+extern char		*fileDiskUUID;
+extern char		*fileDiskType;
+extern int		fileDiskXPos;
+extern int		fileDiskYPos;
+
+extern int		foundDiskNumber;
+extern bool		isDisk;
+
+extern int		xCnt;
+extern int		yCnt;
+
+#endif
