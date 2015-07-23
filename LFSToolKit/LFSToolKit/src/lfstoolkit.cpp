bcdac3f43c7361e37e864adfc05019755a9858dd
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-07-23 13:35:23 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@233 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

--- LFSToolKit/LFSToolKit/src/lfstoolkit.cpp
+++ LFSToolKit/LFSToolKit/src/lfstoolkit.cpp
@@ -1,3 +1,22 @@
+/*
+ *
+ * ©K. D. Hedger. Thu 23 Jul 14:34:11 BST 2015 kdhedger68713@gmail.com
+
+ * This file (lfstoolkit.cpp) is part of LFSToolKit.
+
+ * LFSToolKit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * LFSToolKit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with LFSToolKit.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,24 +35,24 @@ void AAAouttest(char* name)
 }
 
 
-void foo_outHW(char* name)
+void LFSTK_outHW(char* name)
 {
 	printf("Hello World to %s\n",name);
 }
 
-class foo_testclass
+class LFSTK_testclass
 {
 	public:
-		foo_testclass();
-		~foo_testclass();
+		LFSTK_testclass();
+		~LFSTK_testclass();
 		int y;
 };
 
-foo_testclass::~foo_testclass()
+LFSTK_testclass::~LFSTK_testclass()
 {
 }
 
-foo_testclass::foo_testclass()
+LFSTK_testclass::LFSTK_testclass()
 {
 	y=12345;
 }
