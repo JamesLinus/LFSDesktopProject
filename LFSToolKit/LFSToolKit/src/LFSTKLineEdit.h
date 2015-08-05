09aafff482ae237d9fbcf04ce3f58c1ef8c0e8e0
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-08-05 11:38:22 +0000

    added line edit
    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@296 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

---
+++ LFSToolKit/LFSToolKit/src/LFSTKLineEdit.h
@@ -0,0 +1,32 @@
+/*
+ *
+ * ©K. D. Hedger. Wed  5 Aug 12:36:06 BST 2015 kdhedger68713@gmail.com
+
+ * This file (LFSTKLineEdit.h) is part of LFSToolKit.
+
+ * LFSToolKit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * LFSToolKit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with LFSToolKit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef _LFSTKLINEEDIT_
+#define _LFSTKLINEEDIT_
+
+class LFSTK_lineEditclass
+{
+	public:
+		LFSTK_lineEditclass();
+		~LFSTK_lineEditclass();
+};
+
+#endif
