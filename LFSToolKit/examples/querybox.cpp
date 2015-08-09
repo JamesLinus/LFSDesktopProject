09b6927d5bcff21fbe4af984a4b5b1ad7a569f3f
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-08-09 13:05:22 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@317 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

--- LFSToolKit/examples/querybox.cpp
+++ LFSToolKit/examples/querybox.cpp
@@ -2,8 +2,8 @@
 
 #©keithhedger Fri 7 Aug 15:57:52 BST 2015 kdhedger68713@gmail.com
 
-g++ "$0"  -I/tmp/AAA/usr/include -L/tmp/AAA/usr/lib $(pkg-config --cflags --libs x11 xft) -llfstoolkit||exit 1
-./a.out "$@"
+g++ "$0" -I../LFSToolKit/src -L../LFSToolKit/app/.libs $(pkg-config --cflags --libs x11 xft) -llfstoolkit||exit 1
+LD_LIBRARY_PATH=../LFSToolKit/app/.libs ./a.out "$@"
 retval=$?
 rm ./a.out
 exit $retval
