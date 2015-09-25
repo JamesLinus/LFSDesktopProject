b1f9f8d02a0e245bdbcc5484bc69c4f19ba3946e
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-09-25 16:40:00 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@464 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

--- LFSApplications/LFSApplications/src/lfsbackdropprefs.cpp
+++ LFSApplications/LFSApplications/src/lfsbackdropprefs.cpp
@@ -30,18 +30,70 @@
 #include <LFSTKLabel.h>
 #include <LFSTKLib.h>
 
+enum labels {BACKDROP=0,MAINCOLOUR,MONITORBACKDROP,NOMORELABELS};
+enum {EXIT=0,PRINT,APPLY,NOMOREGUIS};
 
 LFSTK_windowClass		*wc=NULL;
 LFSTK_lineEditClass		*backdropPath=NULL;
 LFSTK_toggleButtonClass	*mult=NULL;
 LFSTK_menuButtonClass	*mainMode=NULL;
-LFSTK_lineEditClass		*mainColour=NULL;
+LFSTK_lineEditClass		*mainColourEdit=NULL;
+LFSTK_buttonClass		*guiButtons[NOMOREGUIS]={NULL,};
 
-bool				mainLoop=true;
-int					bwidth=96;
-int					bigbwidth=128;
-int					spacing=bwidth+10;
-int					col1=10,col2=col1+bwidth+spacing+20,col3=col2+bwidth+spacing+20,col4;
+bool					mainLoop=true;
+int						bwidth=96;
+int						bigbwidth=128;
+int						spacing=bwidth+10;
+int						col1=10,col2=col1+bwidth+spacing+20,col3=col2+bwidth+spacing+20,col4;
+char					*prefsPath;
+char					*monitorRCPath;
+
+//prefs
+char					*wallpaperPath;
+int						backdropMode;
+char					*mainColour;
+bool					multiMode;
+
+args					prefs[]=
+{
+	{"backdrop",TYPESTRING,&wallpaperPath},
+	{"mainmode",TYPEINT,&backdropMode},
+	{"colour",TYPESTRING,&mainColour},
+	{"multimode",TYPEBOOL,&multiMode},
+	{NULL,0,NULL}
+};
+
+bool callback(void *p,void* ud)
+{
+
+	switch((long)ud)
+		{
+			case EXIT:
+				
+			wc->LFSTK_clearWindow();
+//			setVars();
+//			wc->globalLib->LFSTK_saveVarsToFile("-",desktopPrefs);
+//			printf("\n");
+			mainLoop=false;
+			return(false);
+			break;
+
+			case PRINT:
+				wc->LFSTK_clearWindow();
+//				setVars();
+//				wc->globalLib->LFSTK_saveVarsToFile("-",desktopPrefs);
+//				printf("\n");
+				break;
+
+			case APPLY:
+//				wc->LFSTK_clearWindow();
+//				setVars();
+//				wc->globalLib->LFSTK_saveVarsToFile(env,desktopPrefs);
+//				system("lfsdesktop &");
+				break;
+		}
+	return(true);
+}
 
 int main(int argc, char **argv)
 {
@@ -54,7 +106,23 @@ int main(int argc, char **argv)
 	FILE*			fp=NULL;
 	char			*command;
 
-	wc=new LFSTK_windowClass(0,0,800,600,"Wallpaper Prefs",false,true);
+	asprintf(&prefsPath,"%s/.config/LFS/lfssetwallpaper.rc",getenv("HOME"));
+	asprintf(&monitorRCPath,"%s/.config/LFS/lfsmonitors.rc",getenv("HOME"));
+
+	wc=new LFSTK_windowClass(0,0,800,600,"Wallpaper Prefs",false);
+	wc->LFSTK_setDecorated(true);
+	geom=wc->LFSTK_getGeom();
+
+	wc->globalLib->LFSTK_loadVarsFromFile(prefsPath,prefs);
+
+	guiButtons[EXIT]=new LFSTK_buttonClass(wc,"Exit",10,geom->h-32,64,24,SouthWestGravity);
+	guiButtons[EXIT]->LFSTK_setCallBack(NULL,callback,(void*)EXIT);
+
+	guiButtons[APPLY]=new LFSTK_buttonClass(wc,"Apply",geom->w-74,geom->h-32,64,24,SouthEastGravity);
+	guiButtons[APPLY]->LFSTK_setCallBack(NULL,callback,(void*)APPLY);
+
+	guiButtons[PRINT]=new LFSTK_buttonClass(wc,"Print",(geom->w/2)-(64/2),geom->h-32,64,24,SouthGravity);
+	guiButtons[PRINT]->LFSTK_setCallBack(NULL,callback,(void*)PRINT);
 
 	//wc->LFSTK_resizeWindow(col3-10,sy);
 	wc->LFSTK_showWindow();
@@ -85,6 +153,11 @@ int main(int argc, char **argv)
 				}
 		}
 
+	for(int j=EXIT;j<NOMOREGUIS;j++)
+		delete guiButtons[j];
+
 	delete wc;
+	free(prefsPath);
+	free(monitorRCPath);
 	return(0);
 }
