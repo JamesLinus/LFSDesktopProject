10f0c5ee3374d8030b84a1fb0c05bc541f80aca5
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-10-12 15:47:48 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@491 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

---
+++ LFSPanel/LFSPanel/src/windowList.cpp
@@ -0,0 +1,31 @@
+/*
+ *
+ * ©K. D. Hedger. Mon 12 Oct 16:45:21 BST 2015 kdhedger68713@gmail.com
+
+ * This file (windowList.cpp) is part of LFSPanel.
+
+ * LFSPanel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * LFSPanel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with LFSPanel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ #include "windowList.h"
+ 
+LFSTK_menuButtonClass	*windowMenu=NULL;
+
+int addWindowMenu(int x,int y,int grav)
+{
+}
+
+void updateWindowMenu(void)
+{
+}
---
+++ LFSPanel/LFSPanel/src/windowList.h
@@ -0,0 +1,32 @@
+/*
+ *
+ * ©K. D. Hedger. Mon 12 Oct 16:45:32 BST 2015 kdhedger68713@gmail.com
+
+ * This file (windowList.h) is part of LFSPanel.
+
+ * LFSPanel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * LFSPanel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with LFSPanel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef _WINDOWLIST_
+#define _WINDOWLIST_
+
+#include "globals.h"
+
+extern LFSTK_menuButtonClass	*windowMenu;
+
+int addWindowMenu(int x,int y,int grav);
+void updateWindowMenu(void);
+
+#endif
