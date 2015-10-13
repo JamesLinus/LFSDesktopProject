ec8d6af2d226605d9742ad76150af04aa14053fc
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-10-13 14:14:24 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@494 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

--- LFSPanel/LFSPanel/src/windowlist.cpp
+++ LFSPanel/LFSPanel/src/windowlist.cpp
@@ -6,7 +6,7 @@
 
  * LFSPanel is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation,either version 3 of the License,or
  * at your option) any later version.
 
  * LFSPanel is distributed in the hope that it will be useful,
@@ -15,34 +15,58 @@
    GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with LFSPanel.  If not, see <http://www.gnu.org/licenses/>.
+ * along with LFSPanel.  If not,see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include "windowlist.h"
-#include "clock.h" 
+#include "clock.h"
 
 LFSTK_menuButtonClass	*windowMenu=NULL;
 menuItemStruct			windowList[MAXWINDOWSINLIST];
 char					windowBuffer[512];
 int						windowListCnt=0;
-//bool					noUpdate=false;
+
+void sendClientMessage(Window win,const char *msg,unsigned long data0,unsigned long data1,unsigned long data2,unsigned long data3,unsigned long data4)
+{
+	XEvent	event;
+	long	mask=SubstructureRedirectMask|SubstructureNotifyMask;
+
+	event.xclient.type=ClientMessage;
+	event.xclient.serial=0;
+	event.xclient.send_event=True;
+	event.xclient.message_type=XInternAtom(mainwind->display,msg,False);
+	event.xclient.window=win;
+	event.xclient.format=32;
+	event.xclient.data.l[0]=data0;
+	event.xclient.data.l[1]=data1;
+	event.xclient.data.l[2]=data2;
+	event.xclient.data.l[3]=data3;
+	event.xclient.data.l[4]=data4;
+
+	XSendEvent(mainwind->display,mainwind->rootWindow,False,mask,&event);
+}
 
 bool windowMenuCB(void *p,void* ud)
 {
-	char buffer[512];
-
 	menuItemStruct	*menu=(menuItemStruct*)ud;
+	Window			winid=0;
+	unsigned long	desktop=0;
+
 	if(menu==NULL)
 		return(true);
 
-	sprintf(buffer,"wmctrl -i -a %p",windowList[menu->subMenuCnt].userData);
-	system(buffer);
+	winid=(Window)(menu->userData);
+	desktop=(unsigned long)menu->subMenuCnt;
+
+	sendClientMessage(mainwind->rootWindow,"_NET_CURRENT_DESKTOP",desktop,0,0,0,0);
+	sendClientMessage(winid,"_NET_ACTIVE_WINDOW",0,0,0,0,0);
+	XMapRaised(mainwind->display,winid);
 	return(true);
 }
 
 void resetMenus(void)
 {
-	for(int j=0;j<windowListCnt;j++)
+	for(int j=0; j<windowListCnt; j++)
 		if(windowList[j].label!=NULL)
 			{
 				free((char*)windowList[j].label);
@@ -70,7 +94,7 @@ void updateWindowMenu(void)
 			if(windowListCnt>MAXWINDOWSINLIST)
 				windowListCnt=MAXWINDOWSINLIST;
 
-			for(int j=0;j<windowListCnt;j++)
+			for(int j=0; j<windowListCnt; j++)
 				{
 					windowBuffer[0]=0;
 					fgets(windowBuffer,511,fp);//id
@@ -83,10 +107,11 @@ void updateWindowMenu(void)
 					windowList[j].label=strdup(windowBuffer);
 
 					windowBuffer[0]=0;
-					fgets(windowBuffer,511,fp);//desktop
+					fgets(windowBuffer,511,fp);//desktop -- TODO
+					windowBuffer[strlen(windowBuffer)-1]=0;
 					windowList[j].bc=NULL;
 					windowList[j].subMenus=NULL;
-					windowList[j].subMenuCnt=j;
+					windowList[j].subMenuCnt=atoi(windowBuffer);
 				}
 			pclose(fp);
 			windowMenu->LFSTK_addMenus(windowList,windowListCnt);
@@ -101,7 +126,6 @@ int addWindowMenu(int x,int y,int grav)
 
 	windowListCnt=-1;
 	updateWindowMenu();
-
 	return(BWIDTH+SPACING);
 }
 
