0d904952522b7be1d669c61b853b7f5c898a97e3
keithhedger <keithhedger@3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f>
2015-11-05 16:41:59 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/LFSDesktopProject@579 3ebf34f5-1e21-47b0-85e2-b17d3fdaf10f

---
+++ LFSToolKit/LFSToolKit/src/LFSTKImage.h
@@ -0,0 +1,14 @@
+/******************************************************
+*
+*     ©keithhedger Thu  5 Nov 16:40:02 GMT 2015
+*     kdhedger68713@gmail.com
+*
+*     LFSTKImage.h
+* 
+******************************************************/
+
+#ifndef _LFSTKIMAGE_
+#define _LFSTKIMAGE_
+
+
+#endif
